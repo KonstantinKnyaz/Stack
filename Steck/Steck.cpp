26fa2b9d88c040423c5fce704200b8ec9ff0b009
Константин Князев <79729833+KonstantinKnyaz@users.noreply.github.com>
2021-03-22 18:05:00 +0300

    Добавьте файлы проекта.

---
+++ Steck/Steck.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <clocale>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+
+	stack<float> st;
+
+	if(st.empty())
+		cout << "Стек пуст" << endl;
+
+	int i = 0;
+	cout << "Заполняем стек" << endl;
+	while(i != 5) {
+		int a;
+		cout << "Введите элемент под номером - " << i + 1 << ": ";
+		cin >> a;
+		st.push(a);
+		i++;
+	}
+
+	/*cout << "Первый элемент: " << st.top() << endl;
+	st.pop();
+	cout << "Первый элемент: " << st.top() << endl;*/
+
+	while (!st.empty()) {
+		cout << "Первый элемент: " << st.top() << endl;
+		st.pop();
+	}
+
+	return 0;
+}
